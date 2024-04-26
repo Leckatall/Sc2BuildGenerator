10baf9aea49932af76b0af2cc6b21ce3de141446
Leckatall <32340119+Leckatall@users.noreply.github.com>
2024-04-26 16:23:16 +0100

    Added Zerg units and am now storing the costs in the Expense dataclass

--- SC2Constants.py
+++ SC2Constants.py
@@ -1,35 +1,53 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Expense:
+    minerals: int
+    vespene: int
+    supply: int
+    build_time: int
+
+
 # Sanitized the data with data lore
-
-
 ZERG_STRUCTURES = {
- 'Baneling Nest': {'Minerals': 100, 'Vespene': '50', 'BuildTime': 43},
- 'Creep Tumor': {'Minerals': 0, 'Vespene': '0', 'BuildTime': 11},
- 'Evolution Chamber': {'Minerals': 75, 'Vespene': '0', 'BuildTime': 25},
- 'Extractor': {'Minerals': 25, 'Vespene': '0', 'BuildTime': 21},
- 'Greater Spire': {'Minerals': 100, 'Vespene': '150', 'BuildTime': 71},
- 'Hatchery': {'Minerals': 300, 'Vespene': '0', 'BuildTime': 71},
- 'Hive': {'Minerals': 200, 'Vespene': '150', 'BuildTime': 71},
- 'Hydralisk Den': {'Minerals': 100, 'Vespene': '100', 'BuildTime': 29},
- 'Infestation Pit': {'Minerals': 100, 'Vespene': '100', 'BuildTime': 36},
- 'Lair': {'Minerals': 150, 'Vespene': '100', 'BuildTime': 57},
- 'Lurker Den': {'Minerals': 100, 'Vespene': '150', 'BuildTime': 57},
- 'Nydus Network': {'Minerals': 150, 'Vespene': '150', 'BuildTime': 36},
- 'Roach Warren': {'Minerals': 150, 'Vespene': '0', 'BuildTime': 39},
- 'Spawning Pool': {'Minerals': 200, 'Vespene': '0', 'BuildTime': 46},
- 'Spine Crawler': {'Minerals': 100, 'Vespene': '0', 'BuildTime': 36},
- 'Spire': {'Minerals': 200, 'Vespene': '200', 'BuildTime': 71},
- 'Spore Crawler': {'Minerals': 75, 'Vespene': '0', 'BuildTime': 21},
- 'Ultralisk Cavern': {'Minerals': 150, 'Vespene': '200', 'BuildTime': 46}
+    'Baneling Nest': Expense(100, 50, 0, 43),
+    'CreepTumor': Expense(0, 0, 0, 11),
+    'Evolution Chamber': Expense(75, 0, 0, 25),
+    'Extractor': Expense(25, 0, 0, 21),
+    'GreaterSpire': Expense(100, 150, 0, 71),
+    'Hatchery': Expense(300, 0, 0, 71),
+    'Hive': Expense(200, 150, 0, 71),
+    'HydraliskDen': Expense(100, 100, 0, 29),
+    'InfestationPit': Expense(100, 100, 0, 36),
+    'Lair': Expense(150, 100, 0, 57),
+    'LurkerDen': Expense(100, 150, 0, 57),
+    'NydusNetwork': Expense(150, 150, 0, 36),
+    'RoachWarren': Expense(150, 0, 0, 39),
+    'SpawningPool': Expense(200, 0, 0, 46),
+    'SpineCrawler': Expense(100, 0, 0, 36),
+    'Spire': Expense(200, 200, 0, 71),
+    'SporeCrawler': Expense(75, 0, 0, 21),
+    'UltraliskCavern': Expense(150, 200, 0, 46)
 }
 
-
-
-
-
-
-
-
-
-
-
-
+ZERG_UNITS = {
+    "Drone": Expense(50, 0, 1, 12),
+    "Overlord": Expense(100, 0, -8, 18),
+    "Queen": Expense(150, 0, 2, 26),
+    "ZerglingPair": Expense(50, 0, 1, 17),
+    "Baneling": Expense(25, 25, 0, 14),
+    "Roach": Expense(75, 25, 2, 19),
+    "Ravager": Expense(25, 75, 1, 12),
+    "Overseer": Expense(50, 50, 0, 12),
+    "Changeling": Expense(0, 0, 0, 0),
+    "Hydralisk": Expense(100, 50, 2, 24),
+    "Lurker": Expense(50, 100, 1, 18),
+    "Mutalisk": Expense(100, 100, 2, 24),
+    "Corruptor": Expense(150, 100, 2, 29),
+    "SwarmHost": Expense(100, 75, 3, 29),
+    "Infestor": Expense(100, 150, 2, 36),
+    "Viper": Expense(100, 200, 3, 29),
+    "Ultralisk": Expense(275, 200, 6, 39),
+    "BroodLord": Expense(150, 150, 2, 24),
+}
